e01150c78fa54f1c7bf48203ead0f54481741362
triono <triono.triono1@gmail.com>
2022-06-22 00:25:31 +0700

    add fn

--- Scripts/JsCrud/crud.js
+++ Scripts/JsCrud/crud.js
@@ -26,3 +26,30 @@
 
 
 }
+
+
+function fnInsertPer() {
+
+    var nmPerusahaan = $('nmPerusahaan').val();
+
+    $.ajax({
+
+        type: "post",
+        url: "/Home/insertPerusahaan/",
+        dataType: "json",
+        contentType: "application/json: charset-utf-8",
+        data: JSON.stringify({
+            nmPerusahaan: nmPerusahaan
+        }),
+        success: function (json) {
+            console.log(json);
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
+        }
+
+    })
+
+
+
+}
